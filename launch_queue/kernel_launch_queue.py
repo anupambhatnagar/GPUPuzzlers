e11564e6ef694585891f51f7e830fa884e442b24
Anupam Bhatnagar <anupamb@fb.com>
2022-12-30 00:30:32 -0800

    fix code

--- launch_queue/kernel_launch_queue.py
+++ launch_queue/kernel_launch_queue.py
@@ -3,40 +3,46 @@ import torch
 from torch.profiler import profile, tensorboard_trace_handler, ProfilerActivity
 
 
-def launch_kernels(large_matrix, small_matrix):
+def launch_kernels(large_matrices, small_matrices):
     # Block 1: execute small matmuls followed by large matmul
-    block1_start = time.time()
-    for _ in range(10):
-       torch.matmul(small_matrix, small_matrix)
-    torch.matmul(large_matrix, large_matrix)
-    block1_end = time.time()
+#    block1_start = time.time_ns()
+    for i in range(10):
+       torch.matmul(small_matrices[i], small_matrices[i])
+    torch.matmul(large_matrices[0], large_matrices[0])
+#    block1_end = time.time_ns()
 
     # sleep
     torch.cuda.synchronize()
     time.sleep(delay)
 
     # Block 2: execute large matmul followed by small matmuls
-    block2_start = time.time()
-    torch.matmul(large_matrix, large_matrix)
-    for _ in range(10):
-       torch.matmul(small_matrix, small_matrix)
-    block2_end = time.time()
+#    block2_start = time.time_ns()
+    torch.matmul(large_matrices[1], large_matrices[1])
+    for j in range(10, 20):
+       torch.matmul(small_matrices[j], small_matrices[j])
+#    block2_end = time.time_ns()
+#
+#    print(f"block 1 start = {block1_start}, block 1 end = {block1_end}, block 2 start = {block2_start}, block 2 end = {block2_end}")
+#    print(f"Block 1 takes = {block1_end - block1_start}, Block 2 takes = {block2_end - block2_start} ")
 
-    print(f"Block 1 takes = {block1_end - block1_start}, Block 2 takes = {block2_end - block2_start} ")
 
+large = 2**12
+small = 2**6
+delay = 0.001 # in seconds
+cuda = torch.device('cuda')
 
-if __name__ == "__main__":
-    large = 2**12
-    small = 2**6
-    delay = 1 # in milliseconds
-    cuda = torch.device('cuda')
+# Create a big and small matrix
+large_matrices = []
+for _ in range(2):
+    large_matrices.append(torch.rand((large, large), device='cuda'))
 
-    # Create a big and small matrix
-    large_matrix = torch.rand((large, large), device='cuda')
-    small_matrix = torch.rand((small, small), device='cuda')
+small_matrices = []
+for _ in range(20):
+    small_matrices.append(torch.rand((small, small), device='cuda'))
 
-    with profile(activities = [ProfilerActivity.CPU, ProfilerActivity.GPU],
-                 trace_handler = tensorboard_trace_handler(dir_name="/tmp", use_gzip=True),
-                 with_stack = True) as prof:
+with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], 
+             on_trace_ready=tensorboard_trace_handler(dir_name=f"./log_{large}_{small}"), 
+             record_shapes=True,
+             with_stack = True) as prof:
 
-        launch_kernels(large_matrix, small_matrix)
+    launch_kernels(large_matrices, small_matrices)
